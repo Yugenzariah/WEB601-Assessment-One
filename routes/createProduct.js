4ad62ff781e537e7a1f8b575c11714c655e87c55
Azariah Keith <yugenzariah@gmail.com>
2024-04-01 18:56:16 +1300

    Added an input for the image url

--- routes/createProduct.js
+++ routes/createProduct.js
@@ -5,8 +5,8 @@ const Product = require('../models/productModel.js');
 // POST create a new product
 router.post('/', async (req, res) => {
   try {
-    const { name, price, image } = req.body; // Extract name, price, and image from request body
-    const product = await Product.create({ name, price, image }); // Include image in the product data
+    const { name, price, image } = req.body; 
+    const product = await Product.create({ name, price, image }); 
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: error.message });
